d186bfda33916518c1f74a6e6e1c303406a8db41
Wagner Rafael | AIExxplorer <202305188631@alunos.estacio.br>
2025-03-02 00:52:28 -0300

    feat: create index.js

---
+++ AIExxplorer_Discord_Bot/src/index.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
+// Inicialização do bot
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent, // Necessário para ler o conteúdo das mensagens
+  ],
+});
+
+// Coleção de comandos (Slash Commands)
+client.commands = new Collection();
+
+// Carregar comandos automaticamente da pasta "commands"
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(path.join(commandsPath, file));
+  client.commands.set(command.data.name, command);
+}
+
+// Carregar eventos automaticamente da pasta "events"
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+  const event = require(path.join(eventsPath, file));
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
+
+// Login do bot
+client.login(process.env.BOT_TOKEN);
